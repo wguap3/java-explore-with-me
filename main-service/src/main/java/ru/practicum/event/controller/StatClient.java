e127cb1e6b7ad35b110d98abcb4f9b4729e3ac8d
3wguap <rnbrell@ya.ru>
2025-10-18 00:14:55 +0400

    2 этап(iter 49)

--- main-service/src/main/java/ru/practicum/event/controller/StatClient.java
+++ main-service/src/main/java/ru/practicum/event/controller/StatClient.java
@@ -10,7 +10,6 @@ import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.dto.ViewStatsDto;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -45,7 +44,7 @@ public class StatClient {
                 .toBodilessEntity()
                 .doOnSuccess(resp -> log.info("Событие отправлено"))
                 .doOnError(err -> log.error("Ошибка отправки события", err))
-                .subscribe();
+                .block();
     }
 
 
@@ -69,7 +68,7 @@ public class StatClient {
                 .toBodilessEntity()
                 .doOnSuccess(resp -> log.info("Событие с ID={} отправлено успешно", id))
                 .doOnError(err -> log.error("Ошибка отправки события с ID={}", id, err))
-                .subscribe();          // асинхронный вызов
+                .block();          // асинхронный вызов
     }
 
 
@@ -80,15 +79,12 @@ public class StatClient {
                                        List<String> uris, boolean unique) {
         log.info("Запрос статистики: start={}, end={}, uris={}, unique={}", start, end, uris, unique);
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String startStr = start.format(formatter);
-        String endStr = end.format(formatter);
 
         return webClient.get()
                 .uri(uriBuilder -> {
                     UriBuilder builder = uriBuilder.path("/stats")
-                            .queryParam("start", startStr)
-                            .queryParam("end", endStr)
+                            .queryParam("start", start)
+                            .queryParam("end", end)
                             .queryParam("unique", unique);
                     if (uris != null && !uris.isEmpty()) {
                         uris.forEach(uri -> builder.queryParam("uris", uri));

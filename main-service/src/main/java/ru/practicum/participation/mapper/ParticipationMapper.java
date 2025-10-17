2b7a08d48dda89bbca419b135bb8a70db46a691b
3wguap <rnbrell@ya.ru>
2025-10-17 21:07:49 +0400

    2 этап(iter 37)

--- main-service/src/main/java/ru/practicum/participation/mapper/ParticipationMapper.java
+++ main-service/src/main/java/ru/practicum/participation/mapper/ParticipationMapper.java
@@ -1,25 +1,21 @@
 package ru.practicum.participation.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
 import ru.practicum.participation.dto.ParticipationDtoOut;
 import ru.practicum.participation.model.Participation;
 
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
-@Mapper(componentModel = "spring")
-public interface ParticipationMapper {
-
-    ParticipationMapper INSTANCE = Mappers.getMapper(ParticipationMapper.class);
-
-    @Mapping(target = "created", expression = "java(formatCreated(participation.getCreated()))")
-    @Mapping(target = "status", source = "status")
-    ParticipationDtoOut mapParticipationToParticipationDtoOut(Participation participation);
-
-    default String formatCreated(java.time.LocalDateTime created) {
-        return created.truncatedTo(ChronoUnit.MILLIS)
-                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS"));
+@Component
+public class ParticipationMapper {
+    public ParticipationDtoOut mapParticipationToParticipationDtoOut(Participation participation) {
+        ParticipationDtoOut participationDtoOut = new ParticipationDtoOut();
+        participationDtoOut.setCreated(participation.getCreated().truncatedTo(ChronoUnit.MILLIS).format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS")));
+        participationDtoOut.setEvent(participation.getEvent());
+        participationDtoOut.setId(participation.getId());
+        participationDtoOut.setRequester(participation.getRequester());
+        participationDtoOut.setStatus(participation.getStatus().toString());
+        return participationDtoOut;
     }
 }

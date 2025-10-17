87ad9b46c6445da1e6e602ce3ef9d97a9f933a29
3wguap <rnbrell@ya.ru>
2025-10-17 22:03:21 +0400

    2 этап(iter 43)

--- main-service/src/main/java/ru/practicum/participation/mapper/ParticipationMapper.java
+++ main-service/src/main/java/ru/practicum/participation/mapper/ParticipationMapper.java
@@ -1,31 +1,22 @@
 package ru.practicum.participation.mapper;
 
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
 import ru.practicum.participation.dto.ParticipationDtoOut;
 import ru.practicum.participation.model.Participation;
 
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
-@Mapper(componentModel = "spring")
-public interface ParticipationMapper {
-
-    @Mapping(
-            target = "created",
-            expression = "java(formatCreated(participation.getCreated()))"
-    )
-    @Mapping(target = "event", source = "event")
-    @Mapping(target = "id", source = "id")
-    @Mapping(target = "requester", source = "requester")
-    @Mapping(target = "status", expression = "java(participation.getStatus().toString())")
-    ParticipationDtoOut mapParticipationToParticipationDtoOut(Participation participation);
-
-    default String formatCreated(java.time.LocalDateTime created) {
-        if (created == null) return null;
-        return created
-                .truncatedTo(ChronoUnit.MILLIS)
-                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS"));
+@Component
+public class ParticipationMapper {
+    public ParticipationDtoOut mapParticipationToParticipationDtoOut(Participation participation) {
+        ParticipationDtoOut participationDtoOut = new ParticipationDtoOut();
+        participationDtoOut.setCreated(participation.getCreated().truncatedTo(ChronoUnit.MILLIS).format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS")));
+        participationDtoOut.setEvent(participation.getEvent());
+        participationDtoOut.setId(participation.getId());
+        participationDtoOut.setRequester(participation.getRequester());
+        participationDtoOut.setStatus(participation.getStatus().toString());
+        return participationDtoOut;
     }
 }

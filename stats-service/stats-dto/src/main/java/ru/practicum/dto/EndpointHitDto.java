8a818f03bde98e3312ba4537d1d73a77dc2ad000
3wguap <rnbrell@ya.ru>
2025-10-19 22:53:11 +0400

    2 этап(iter 72)

--- stats-service/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
+++ stats-service/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -1,11 +1,11 @@
 package ru.practicum.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -26,6 +26,6 @@ public class EndpointHitDto {
     private String ip;
 
     @NotNull
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }

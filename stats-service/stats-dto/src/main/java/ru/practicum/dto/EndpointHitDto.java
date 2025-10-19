b384998af912d8db504bdd626d8192eb41383994
3wguap <rnbrell@ya.ru>
2025-10-19 22:30:55 +0400

    2 этап(iter 70)

--- stats-service/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
+++ stats-service/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -1,12 +1,10 @@
 package ru.practicum.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
